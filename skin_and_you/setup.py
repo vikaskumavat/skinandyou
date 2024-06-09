407d8761f33bfbb728870659f82d946ac2a173ce
vikaskumavat <98009070+vikaskumavat@users.noreply.github.com>
2024-06-09 20:32:07 +0530

    Add files via upload

---
+++ skin_and_you/setup.py
@@ -0,0 +1,19 @@
+sudo -i -u postgres
+
+
+create user skin_user with encrypted password 'skin@9987#!';
+
+
+grant all privileges on database skin_n_you to skin_user;
+
+
+
+
+/var/www/python_envs/skin_and_you/bin   
+
+
+
+bnkpg2244q25041995
+
+
+bnkpg2244q25041995
